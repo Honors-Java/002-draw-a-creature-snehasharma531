c366153cfd8e643540a9de335a0be6086570c21d
sneha sharma <PrincessSneha@gmail.com>
2020-09-15 15:25:28 +0000

    completed draw the creature

--- index.java
+++ index.java
@@ -11,8 +11,32 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+  background(255,255,0);
   
- 
+   fill(0);
+   stroke(0);
+   ellipse(150,200,125,120);
+   fill(255);
+   stroke(0);
+   ellipse(150,120,70,70);
+   fill(255,0,0);
+   stroke(0);
+   rect(132,108,10,8);
+   rect(162,108,10,8);
+   stroke(100);
+   line(145,125,152,125,);
+   line(145,125,148,134);
+   line(152,125,148,134);
+   fill(52,235,255);
+   stroke(0)
+   rect(130,265,20,20);
+   rect(160,265,20,20);
+   stroke(0,0,255);
+   line(90,186,65,220);
+   line(208,180,240,215);
+   
+   
+   
   
   
  
